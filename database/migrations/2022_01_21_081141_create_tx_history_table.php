e4afd93875e35437d38151311e59ca941499408a
frnlx <55038085+frnlx@users.noreply.github.com>
2022-01-21 16:35:27 +0700

    Add files via upload

---
+++ database/migrations/2022_01_21_081141_create_tx_history_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTxHistoryTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tx_history', function (Blueprint $table) {
+            $table->id('txid');
+            $table->foreignId('id')->constrained('tx_history_header');
+            $table->foreignId('keyboard_id')->constrained('keyboards');
+            $table->integer('quantity');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tx_history');
+    }
+}
